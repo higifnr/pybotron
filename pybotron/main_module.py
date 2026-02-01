e3070bad294fe3574092b4e60fbf887c8ec03e68
higifnr <abdelhakimboubaker6@gmail.com>
2026-02-01 09:05:55 -0800

    Hotfix.

--- pybotron/main_module.py
+++ pybotron/main_module.py
@@ -13,6 +13,7 @@ from scipy.linalg import expm, logm
 # --- Plotting / Visualization ---
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.animation import FuncAnimation
 from mpl_toolkits.mplot3d.art3d import Line3D
 import cv2
 
